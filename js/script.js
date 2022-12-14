8f3cf2368a2ea2c6a14d7992df656fc4ca4daa4f
Yevhen <melnyk_evgenij@icloud.com>
2022-12-14 15:53:29 +0200

    add question to project

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-"use strict";
+ "use strict";
 
  
 
-let number = 5;
+/*let number = 5;
 const leftBorderWidth = 1;
 
 
@@ -15,4 +15,24 @@ const obj = {
 
 obj.a = 10;
 
-console.log(obj);
+console.log(obj); */
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+const a = prompt('Одни из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Одни из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
